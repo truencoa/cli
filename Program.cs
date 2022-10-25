c96ec3550755666631b74ed897b9392bd55c5135
Tim Hunnewell <thunnewell@informitics.com>
2022-10-25 13:57:01 -0500

    created solution

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace truencoa_cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
